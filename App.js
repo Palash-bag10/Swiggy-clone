37dcabb21d0628df31e763252dd32272e95c2468
Palash Bag <palashbag903@gmail.com>
2024-01-29 19:42:57 +0530

    build hello world using HTML, JS and REACT

---
+++ App.js
@@ -0,0 +1,90 @@
+// const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+// How to create nested HTML structure inside React
+/**
+ * 
+ *  <div id="parent">
+ *      <div id="child">
+ *          <h1>I'm h1 tag</h1>
+ *      </div>
+ *  </div>
+ * 
+ */
+
+// const parent = React.createElement(
+//     "div",
+//     {id: "parent"},
+//     React.createElement(
+//         "div",
+//         {id: "child"},
+//         React.createElement(
+//             "h1",
+//             {},
+//             "I'm h1 tag"
+//         )
+//     )
+// )
+
+// How to create siblings inside React
+/**
+ * 
+ *  <div id="parent">
+ *      <div id="child">
+ *          <h1>I'm h1 tag</h1>
+ *          <h2>I'm h2 tag</h2>
+ *      </div>
+ *  </div>
+ * 
+ */
+
+// const parent = React.createElement(
+//     "div",
+//     {id: "parent"},
+//     React.createElement(
+//         "div",
+//         {id: "child"}, [
+//             React.createElement("h1", {}, "I'm h1 tag"),
+//             React.createElement("h2", {}, "I'm h2 tag"),
+//         ]
+//     )
+// )
+
+/**
+ * 
+ *  <div id="parent">
+ *      <div id="child">
+ *          <h1>I'm h1 tag</h1>
+ *          <h2>I'm h2 tag</h2>
+ *      </div>
+ *      <div id="child2">
+ *          <h1>I'm h1 tag</h1>
+ *          <h2>I'm h2 tag</h2>
+ *      </div>
+ *  </div>
+ * 
+ */
+
+const parent = React.createElement(
+    "div",
+    {id: "parent"},
+    [
+        React.createElement("div", {id: "child"}, [
+            React.createElement("h1", {}, "I'm h1 tag"),
+            React.createElement("h2", {}, "I'm h2 tag"),
+        ]),
+        React.createElement("div", {id: "child2"}, [
+            React.createElement("h1", {}, "I'm h1 tag"),
+            React.createElement("h2", {}, "I'm h2 tag"),
+        ])   
+    ]
+)
+
+console.log(parent)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
