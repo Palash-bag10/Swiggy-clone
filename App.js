3b173b9b605e07f78fa01ef58e20a565404980de
Palash Bag <palashbag903@gmail.com>
2024-02-01 19:06:19 +0530

    03 - understanding JSX and React Component

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react"
+import ReactDOM  from "react-dom/client";
+
 // const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -68,23 +71,75 @@
  * 
  */
 
-const parent = React.createElement(
-    "div",
-    {id: "parent"},
-    [
-        React.createElement("div", {id: "child"}, [
-            React.createElement("h1", {}, "I'm h1 tag"),
-            React.createElement("h2", {}, "I'm h2 tag"),
-        ]),
-        React.createElement("div", {id: "child2"}, [
-            React.createElement("h1", {}, "I'm h1 tag"),
-            React.createElement("h2", {}, "I'm h2 tag"),
-        ])   
-    ]
-)
+// const parent = React.createElement(
+//     "div",
+//     {id: "parent"},
+//     [
+//         React.createElement("div", {id: "child"}, [
+//             React.createElement("h1", {}, "I'm h1 tag"),
+//             React.createElement("h2", {}, "I'm h2 tag"),
+//         ]),
+//         React.createElement("div", {id: "child2"}, [
+//             React.createElement("h1", {}, "I'm h1 tag"),
+//             React.createElement("h2", {}, "I'm h2 tag"),
+//         ])   
+//     ]
+// )
+
+// const jsxHeading = (
+//     <h1>Namaste React using JSX 🚀</h1>
+// );
+
+const Title = () => (
+    <h1>Namaste React using JSX 🚀</h1>
+);
+
+// React Functional Component
+const HeadingComponent = () => {
+    return <h1>Namaste React Functional Component</h1>
+} 
+
+// const HeadingComponent2 = () => (
+//     <div id="container">
+//         <h1>Namaste React Functional Component</h1>
+//     </div>
+// );
+
+// Render Title component inside container
+// const HeadingComponent2 = () => (
+//     <div id="container">
+//         <Title/>
+//         <h1>Namaste React Functional Component</h1>
+//     </div>
+// );
+
+// // render js code inside component
+// const number = 1000;
+
+// const HeadingComponent2 = () => (
+//     <div id="container">
+//         <h2>{number}</h2>
+//         <h1>Namaste React Functional Component</h1>
+//     </div>
+// );
+
+// Render element inside component
+const title = (
+    <h1>Namaste React using JSX 🚀</h1>
+);
+
+const HeadingComponent2 = () => (
+    <div id="container">
+        {title}
+        <h1>Namaste React Functional Component</h1>
+    </div>
+);
 
 console.log(parent)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+// root.render(parent);
+// root.render(jsxHeading);
+// Render component
+root.render(< HeadingComponent2 />)
