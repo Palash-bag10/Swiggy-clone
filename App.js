e5379375abd6b9a18f461034c9c5dd862d23bd6c
Palash Bag <palashbag903@gmail.com>
2024-02-02 12:42:03 +0530

    build restaurent card dynamically using swiggy api

--- App.js
+++ App.js
@@ -1,77 +1,333 @@
-import React from "react"
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <div className="logo-container">
-                <img
-                    className="logo"
-                    src="https://w7.pngwing.com/pngs/664/210/png-transparent-uber-eats-muncheez-delivery-online-food-ordering-food-delivery-food-logo-uber-eats.png"
-                    alt="app logo" />
-            </div>
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://w7.pngwing.com/pngs/664/210/png-transparent-uber-eats-muncheez-delivery-online-food-ordering-food-delivery-food-logo-uber-eats.png"
+          alt="app logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 const styleCard = {
-    backgroundColor: "lightgreen"
-}
+  backgroundColor: "lightgreen",
+};
 
-const RestaurantCard = () => {
-    return (
-        <div className="restaurant-card" style={styleCard}>
-            <img
-             src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ub35uaf2iferroprejgy" 
-             alt="res-logo"
-             className="res-logo"
-            />
-            <h3>Maghna Foods</h3>
-            <p>Biriyani, Asian</p>
-            <p>4.4 stars</p>
-            <p>40 mins</p>
-        </div>
-    )
-}
+const RestaurantCard = ({ resObj }) => {
+  return (
+    <div className="restaurant-card" style={styleCard}>
+      <img
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          resObj.info.cloudinaryImageId
+        }
+        alt="res-logo"
+        className="res-logo"
+      />
+      <h3> {resObj.info.name} </h3>
+      <h5> {resObj.info.cuisines.join(", ")} </h5>
+      <h5>{resObj.info.avgRating} stars</h5>
+      <h5>{resObj.info.costForTwo}</h5>
+      <h5>{resObj.info.sla.deliveryTime} mins</h5>
+    </div>
+  );
+};
+
+const resObj = [
+  {
+    info: {
+      id: "341676",
+      name: "Wow! Momo",
+      cloudinaryImageId: "5a148e63e9c54942e37627da1aa156be",
+      locality: "Srerampore",
+      areaName: "Srerampore",
+      costForTwo: "₹300 for two",
+      cuisines: [
+        "Tibetan",
+        "Healthy Food",
+        "Asian",
+        "Chinese",
+        "Snacks",
+        "Continental",
+        "Desserts",
+        "Beverages",
+      ],
+      avgRating: 4.5,
+      parentId: "1776",
+      avgRatingString: "4.5",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 38,
+        lastMileTravel: 3.6,
+        serviceability: "SERVICEABLE",
+        slaString: "35-40 mins",
+        lastMileTravelString: "3.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-02-02 23:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹150 OFF",
+        subHeader: "ABOVE ₹499",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/wow-momo-srerampore-kolkata-341676",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "122174",
+      name: "Mio Amore - The Cake Shop",
+      cloudinaryImageId: "8bf25e6cafe30c7e7c9c8f31f628fe96",
+      locality: "Rb Sarani",
+      areaName: "Serampore",
+      costForTwo: "₹150 for two",
+      cuisines: ["Desserts", "Bakery", "Snacks"],
+      avgRating: 4.6,
+      parentId: "292958",
+      avgRatingString: "4.6",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 33,
+        lastMileTravel: 4.9,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "4.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-02-02 20:00:00",
+        opened: true,
+      },
+      badges: {
+        textExtendedBadges: [
+          {
+            iconId: "v1705582451/Ratnesh_Badges/Listing_HR.png",
+            shortDescription: "Perfect Cake Delivery",
+            fontColor: "#7E808C",
+          },
+        ],
+      },
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "",
+                  fontColor: "#7E808C",
+                  iconId: "v1705582451/Ratnesh_Badges/Listing_HR.png",
+                  shortDescription: "Perfect Cake Delivery",
+                },
+              },
+            ],
+          },
+        },
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/mio-amore-the-cake-shop-rb-sarani-serampore-kolkata-122174",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "203964",
+      name: "KFC",
+      cloudinaryImageId: "f01666ac73626461d7455d9c24005cd4",
+      locality: "Grand Trunk Road",
+      areaName: "Serampore",
+      costForTwo: "₹450 for two",
+      cuisines: ["Burgers", "Biryani", "American", "Snacks", "Fast Food"],
+      avgRating: 4.3,
+      parentId: "547",
+      avgRatingString: "4.3",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 45,
+        lastMileTravel: 5.8,
+        serviceability: "SERVICEABLE",
+        slaString: "40-45 mins",
+        lastMileTravelString: "5.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-02-02 23:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "40% OFF",
+        subHeader: "UPTO ₹80",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/kfc-grand-trunk-road-serampore-kolkata-203964",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "579971",
+      name: "Vadilal Ice Creams",
+      cloudinaryImageId: "edf6aa61ed4cdfcda130897c5b3e5255",
+      areaName: "Bally",
+      costForTwo: "₹250 for two",
+      cuisines: ["Ice Cream", "Desserts"],
+      avgRating: 4.7,
+      veg: true,
+      parentId: "11745",
+      avgRatingString: "4.7",
+      totalRatingsString: "50+",
+      sla: {
+        deliveryTime: 70,
+        lastMileTravel: 10.4,
+        serviceability: "SERVICEABLE",
+        slaString: "70-75 mins",
+        lastMileTravelString: "10.4 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-02-02 23:30:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/vadilal-ice-creams-bally-kolkata-579971",
+      type: "WEBLINK",
+    },
+  },
+];
 
 const Body = () => {
-    return (
-        <div className="body">
-            <div className="search"> Search </div>
-            <div className="restaurant-container">
-                {/* RestaurantCard */}
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="body">
+      <div className="search"> Search </div>
+      <div className="restaurant-container">
+        {/* RestaurantCard */}
+        <RestaurantCard resObj={resObj[0]} />
+        <RestaurantCard resObj={resObj[1]} />
+        <RestaurantCard resObj={resObj[2]} />
+        <RestaurantCard resObj={resObj[3]} />
+        {/* <RestaurantCard resName="KFC" cuisine="Burger, Fast Food" /> */}
+      </div>
+    </div>
+  );
+};
 
 const AppLayout = () => {
-    return (
-        <div className="app">
-            {/* Header */}
-            <Header />
-            {/* Body */}
-            <Body />
-        </div>
-    )
-}
-
-
+  return (
+    <div className="app">
+      {/* Header */}
+      <Header />
+      {/* Body */}
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(< AppLayout />)
+root.render(<AppLayout />);
