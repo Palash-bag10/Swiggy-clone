e75ed3d76a2cde8bb4e022e5abce42fb640a412e
Palash Bag <palashbag903@gmail.com>
2024-02-04 13:05:00 +0530

    handled error in a better way using useRouteError hook

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,8 +1,15 @@
+import { useRouteError } from "react-router-dom";
+
 const Error = () => {
+
+    const err = useRouteError();
+    console.log(err)
+
     return (
         <div>
             <h1>OPPS!!!</h1>
             <h2>Something Went Wrong!!!</h2>
+            <h3> {err.status} : {err.statusText} </h3>
         </div>
     )
 }
