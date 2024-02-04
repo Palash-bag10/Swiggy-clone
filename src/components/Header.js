37a90ccb03fed7e6e7ad94475a3ae0ba8720fded
Palash Bag <palashbag903@gmail.com>
2024-02-04 17:38:45 +0530

    created link to pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const Header = () => {
 
@@ -16,10 +17,10 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact</li>
-          <li>Cart</li>
+          <li> <Link to="/">Home</Link> </li>
+          <li> <Link to="/about">About Us</Link> </li>
+          <li> <Link to="/contact">Contact</Link> </li>
+          <li> <Link to="/">Cart</Link> </li>
           <button
           className="login"
           onClick={() => {
