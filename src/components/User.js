9c1cc9a06a8dc8854911afd387bbb0107e1f24b4
Palash Bag <palashbag903@gmail.com>
2024-02-05 12:37:45 +0530

    created state variable inside class based comp and functional comp in about us page

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,14 @@
+import { useState } from "react";
+
 const User = ({name}) => {
+
+    const [count] = useState(0)
+    const [count2] = useState(1)
+
     return (
         <div className="user-card">
+            <h1>Count =  {count} </h1>
+            <h1>Count2 =  {count2} </h1>
             <h2>Name: {name}</h2>
             <h3>Location: Kolkata</h3>
             <h5>Contact: palash-bag10</h5>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,11 +4,18 @@ class UserClass extends React.Component {
 
     constructor(props){
         super(props)
+
+        this.state = {
+            count: 0,
+            count2: 2,
+        }
     }
 
     render() {
         return (
             <div className="user-card">
+                <h1> Count: {this.state.count} </h1>
+                <h1> Count2: {this.state.count2} </h1>
                 <h2>Name: {this.props.name}</h2>
                 <h3>Location: Kolkata</h3>
                 <h5>Contact: palash-bag10</h5>
