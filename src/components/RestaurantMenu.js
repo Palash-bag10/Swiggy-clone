dfee80d3cdbe0d9254f885d36a88914a6f158f5d
Palash Bag <palashbag903@gmail.com>
2024-02-06 01:15:45 +0530

    created custome hook for fetch restaurant menu data

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,26 +1,12 @@
-import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import { MENU_URL } from "../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
 
-    const [resInfo, setResInfo] = useState(null);
     const {resId} = useParams();
 
-    useEffect(() => {
-        fetchMenu();
-    },[])
-
-    const fetchMenu = async() => {
-        const data = await fetch(MENU_URL+resId);
-
-        const json = await data.json();
-
-        console.log(json);
-        setResInfo(json.data);
-
-    }
+    const resInfo = useRestaurantMenu(resId)
 
     if(resInfo === null) return <Shimmer/>
 
@@ -38,7 +24,6 @@ const RestaurantMenu = () => {
                 {itemCards.map((item) => (
                     <li key={item.card.info.id}> 
                         {item.card.info.name} - {"Rs."} {item.card.info.price / 100 || item.card.info.defaultPrice / 100} 
-                        {/* <p>  </p>  */}
                     </li>
                 ))}
             </ul>
