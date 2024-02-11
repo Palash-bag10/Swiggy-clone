74144fe1e51f6cfb04c78cea4024301ad9dfa205
Palash Bag <palashbag903@gmail.com>
2024-02-11 12:03:40 +0530

    wrote test case for contact page heading

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,24 @@
 const Contact = () => {
-    return (
-        <div>
-            <h1>This is Contact Us Page</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1 className="font-bold text-3xl p-4 m-4">Contact Us Page</h1>
+      <form>
+        <input
+          type="text"
+          className=" border border-black p-2 m-2"
+          placeholder="name"
+        />
+        <input
+          type="text"
+          className=" border border-black p-2 m-2"
+          placeholder="message"
+        />
+        <button className=" border border-black p-2 m-2 bg-gray-100 rounded-lg">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Contact;
